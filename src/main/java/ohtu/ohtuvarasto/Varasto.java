3d0609548975dca85448b0a6741b175b0ca74929
Tuomas <tietotuomas@gmail.com>
2021-02-10 23:59:44 +0200

    Break checkstyle

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -7,7 +7,8 @@ public class Varasto {
     private double saldo;     // paljonko varastossa on nyt, >= 0
 
     // --- konstruktorit: ---
-    public Varasto(double tilavuus) {  // tilavuus on annettava
+    public Varasto(double tilavuus) 
+    {  // tilavuus on annettava
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
         } else { // virheellinen, nollataan
